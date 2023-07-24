cd94e187a5e95e71a0e94288d02366aafd8dd97a
andreashenc <andreas@encord.com>
2023-07-24 16:01:23 +0100

    Prop bbox on all slices

--- anno2dcm.py
+++ anno2dcm.py
@@ -11,7 +11,7 @@ with Path(keyfile).open() as f:
     private_key = f.read()
 user_client = EncordUserClient.create_with_ssh_private_key(private_key)
 
-project_hashes = ['20e8f07f-30a4-4840-836d-6633426d4d49']
+project_hashes = [INSERT_YOUR_PROJECT_HASH]
 
 output_path = './dicom/'
 
@@ -19,10 +19,36 @@ isExist = os.path.exists(output_path)
 if not isExist:
    os.makedirs(output_path)
 
-res = []
+
+annotations_storage = {}
+# Store annotation in dict keyed by the data_hash
+
+
+"""
+    First, cache all the annotations in a project
+"""
+for p_hash in project_hashes:
+    project = user_client.get_project(p_hash)    
+    for label_row in project.list_label_rows():
+            lr = project.get_label_row(label_row.label_hash, get_signed_url=True)
+            with tqdm(total=len(list(lr['data_units'].values())[0]['labels'].values())) as pbar:
+                for dicom_slice in list(lr['data_units'].values())[0]['labels'].values():
+                    # Check if annotations exist
+                    if len(dicom_slice['objects'])>0:
+                        if lr.data_hash in annotations_storage.keys():     
+                            annos = annotations_storage[lr.data_hash]
+                            annos.append(dicom_slice)
+                            annotations_storage[lr.data_hash]=annos
+                        else:
+                            annotations_storage.update({lr.data_hash:[dicom_slice]})                      
+                    pbar.update(1)
+
+"""
+    Now in the second pass we propagate all found bounding boxes to all slices 
+"""
+
 for p_hash in project_hashes:
     project = user_client.get_project(p_hash)
-    
     for label_row in project.list_label_rows():
             lr = project.get_label_row(label_row.label_hash, get_signed_url=True)
             with tqdm(total=len(list(lr['data_units'].values())[0]['labels'].values())) as pbar:
@@ -39,19 +65,21 @@ for p_hash in project_hashes:
                         _ = f.write(r.content)
 
                     # Check if annotations exist
-                    if len(dicom_slice['objects'])>0:
-                        # Annotations found on the slice
-                        for bb in dicom_slice['objects']:
-                            if 'boundingBox' in bb.keys():
-                                x1 = round(bb['boundingBox']['x'] * w)
-                                y1 = round(bb['boundingBox']['y'] * h)
-                                x2 = x1 + round(bb['boundingBox']['w'] * w)
-                                y2 = y1 + round(bb['boundingBox']['h'] * h)
-                            dcm = pydicom.read_file(os.path.join(output_dirname,output_filename))
-                            # Zero out pixels inside bounding box
-                            dcm.pixel_array[y1:y2,x1:x2] = 0
-                            redacted_pixeldata = dcm.pixel_array.tobytes()
-                            dcm.PixelData = redacted_pixeldata
-                            # Store redacted file
-                            dcm.save_as(os.path.join(output_dirname,output_filename))
+                    if lr.data_hash in annotations_storage.keys():
+                        # We have annotations for the slice ...
+                        dcm = pydicom.read_file(os.path.join(output_dirname,output_filename))
+                        for dicom_slice in annotations_storage[lr.data_hash]:
+                            # Annotations found on the slice
+                            for bb in dicom_slice['objects']:
+                                if 'boundingBox' in bb.keys():
+                                    x1 = round(bb['boundingBox']['x'] * w)
+                                    y1 = round(bb['boundingBox']['y'] * h)
+                                    x2 = x1 + round(bb['boundingBox']['w'] * w)
+                                    y2 = y1 + round(bb['boundingBox']['h'] * h)
+                                # Zero out pixels inside bounding box
+                                dcm.pixel_array[y1:y2,x1:x2] = 0
+                                redacted_pixeldata = dcm.pixel_array.tobytes()
+                                dcm.PixelData = redacted_pixeldata
+                                # Store redacted file
+                        dcm.save_as(os.path.join(output_dirname,output_filename))
                     pbar.update(1)
