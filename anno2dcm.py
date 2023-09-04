8b75746c807d656880554528ff4498a96de27079
alex-encord <alexandre@encord.com>
2023-09-04 15:51:22 +0100

    Refactor to use only one pass on lrs and another pass on cached data.

--- anno2dcm.py
+++ anno2dcm.py
@@ -25,90 +25,74 @@ isExist = os.path.exists(output_path)
 if not isExist:
     os.makedirs(output_path)
 
-# Store annotation in dict keyed by the data_hash
-annotations_storage = defaultdict(list)
-metadata_storage = {}
-
-
-"""
-    First, cache all the annotations in a project
-"""
 for p_hash in project_hashes:
     project = user_client.get_project(p_hash)
     for label_row in tqdm(project.list_label_rows()):
         lr = project.get_label_row(label_row.label_hash, get_signed_url=True)
         labels = list(lr['data_units'].values())[0]['labels'].values()
+
+        # Extract all annotations from the series
+        bboxes = []
+        metadata = []
+
         for dicom_slice in labels:
             data_hash = lr.data_hash
+            metadata.append({
+                'signed_url': dicom_slice['metadata']['file_uri'],
+                'filename': dicom_slice['metadata']['dicom_instance_uid'] + '.dcm'
+            })
             # Check if annotations exist
             if len(dicom_slice['objects']) > 0:
-                annotations_storage[data_hash].append(dicom_slice)
-            metadata_storage[data_hash] = {
-                'w': dicom_slice['metadata']['width'],
-                'h': dicom_slice['metadata']['height']
-            }
+                for bb in dicom_slice['objects']:
+                    if 'boundingBox' in bb.keys():
+                        w = dicom_slice['metadata']['width']
+                        h = dicom_slice['metadata']['height']
+                        x1 = round(bb['boundingBox']['x'] * w)
+                        y1 = round(bb['boundingBox']['y'] * h)
+                        bboxes.append({
+                            'x1': x1,
+                            'y1': y1,
+                            'x2': x1 + round(bb['boundingBox']['w'] * w),
+                            'y2': y1 + round(bb['boundingBox']['h'] * h)
+                        })
 
-"""
-    Now in the second pass we propagate all found bounding boxes to all slices 
-"""
-
-for p_hash in project_hashes:
-    project = user_client.get_project(p_hash)
-    for label_row in project.list_label_rows():
-        lr = project.get_label_row(label_row.label_hash, get_signed_url=True)
-        with tqdm(
-            total=len(list(lr['data_units'].values())[0]['labels'].values())
-        ) as pbar:
-            for dicom_slice in list(lr['data_units'].values())[0]['labels'].values():
-                w = metadata_storage[lr.data_hash]['w']
-                h = metadata_storage[lr.data_hash]['h']
-                # First save slice
-                r = requests.get(dicom_slice['metadata']['file_uri'])
-                output_dirname = os.path.join(os.path.join(output_path, lr.data_hash))
-                output_filename = dicom_slice['metadata']['dicom_instance_uid'] + '.dcm'
-                if not os.path.exists(output_dirname):
-                    os.makedirs(output_dirname)
+        # If there are annotations, download series and perform redaction
+        if len(bboxes) > 0:
+            output_dirname = os.path.join(os.path.join(output_path, lr.data_hash))
+            if not os.path.exists(output_dirname):
+                os.makedirs(output_dirname)
+            for meta in metadata:
+                r = requests.get(meta['signed_url'])
+                output_filename = meta['filename']
                 with open(os.path.join(output_dirname, output_filename), 'wb') as f:
-                    _ = f.write(r.content)
-
-                # Check if annotations exist
-                if lr.data_hash in annotations_storage.keys():
-                    # We have annotations for the slice ...
-                    dcm = pydicom.read_file(
-                        os.path.join(output_dirname, output_filename)
-                    )
-                    if (
+                    f.write(r.content)
+                dcm = pydicom.read_file(
+                    os.path.join(output_dirname, output_filename)
+                )
+                if (
                         dcm.file_meta.TransferSyntaxUID == pydicom.uid.JPEG2000Lossless
                         and dcm.BitsStored == 12
+                ):
+                    dcm.BitsStored = 16
+                for bbox in bboxes:
+                    # Zero out pixels inside bounding box
+                    dcm.pixel_array[bbox['y1']:bbox['y2'], bbox['x1']:bbox['x2']] = 0
+                    if (
+                            dcm.file_meta.TransferSyntaxUID
+                            == pydicom.uid.JPEG2000Lossless
                     ):
-                        dcm.BitsStored = 16
-                    for dicom_slice in annotations_storage[lr.data_hash]:
-                        # Annotations found on the slice
-                        for bb in dicom_slice['objects']:
-                            if 'boundingBox' in bb.keys():
-                                x1 = round(bb['boundingBox']['x'] * w)
-                                y1 = round(bb['boundingBox']['y'] * h)
-                                x2 = x1 + round(bb['boundingBox']['w'] * w)
-                                y2 = y1 + round(bb['boundingBox']['h'] * h)
-                            # Zero out pixels inside bounding box
-                            dcm.pixel_array[y1:y2, x1:x2] = 0
-                            if (
-                                dcm.file_meta.TransferSyntaxUID
-                                == pydicom.uid.JPEG2000Lossless
-                            ):
-                                encoded = jpeg2k_encode(dcm.pixel_array, level=0)
-                                dcm.PixelData = pydicom.encaps.encapsulate([encoded])
-                            else:
-                                redacted_pixeldata = dcm.pixel_array.tobytes()
-                                dcm.PixelData = redacted_pixeldata
-                    # Store redacted file
-                    f = os.path.join(output_dirname, output_filename)
-                    # Store locally
-                    dcm.save_as(f)
-                    # Store in bucket
-                    client.upload_file(
-                        f,
-                        bucket_name,
-                        os.path.join(bucket_folder, lr.data_title, output_filename),
-                    )
-                pbar.update(1)
+                        encoded = jpeg2k_encode(dcm.pixel_array, level=0)
+                        dcm.PixelData = pydicom.encaps.encapsulate([encoded])
+                    else:
+                        redacted_pixeldata = dcm.pixel_array.tobytes()
+                        dcm.PixelData = redacted_pixeldata
+                # Store redacted file
+                f = os.path.join(output_dirname, output_filename)
+                # Store locally
+                dcm.save_as(f)
+                # Store in bucket
+                client.upload_file(
+                    f,
+                    bucket_name,
+                    os.path.join(bucket_folder, lr.data_title, output_filename),
+                )
