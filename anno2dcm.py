c4b2cdd88de3468f65a8c9b6c5f6ecc08be9903f
alex-encord <alexandre@encord.com>
2023-09-04 16:46:10 +0100

    Linting.

--- anno2dcm.py
+++ anno2dcm.py
@@ -3,24 +3,29 @@ import shutil
 from typing import List, Dict, Tuple
 
 import boto3
-import pydicom
 import requests
 from encord import EncordUserClient
 from imagecodecs import jpeg2k_encode
-from pydicom import FileDataset
+from pydicom import read_file, FileDataset
+from pydicom.encaps import encapsulate
+from pydicom.uid import JPEG2000Lossless
 from tqdm import tqdm
 
 
-def get_redaction_bboxes_and_metadata(labels: List[Dict]) -> Tuple[List[Dict], List[Dict]]:
+def get_redaction_bboxes_and_metadata(
+    labels: List[Dict],
+) -> Tuple[List[Dict], List[Dict]]:
     # Extract all annotations from the series
     bboxes = []
     metadata = []
 
     for dicom_slice in labels:
-        metadata.append({
-            'signed_url': dicom_slice['metadata']['file_uri'],
-            'filename': dicom_slice['metadata']['dicom_instance_uid'] + '.dcm'
-        })
+        metadata.append(
+            {
+                'signed_url': dicom_slice['metadata']['file_uri'],
+                'filename': dicom_slice['metadata']['dicom_instance_uid'] + '.dcm',
+            }
+        )
         # Check if annotations exist
         if len(dicom_slice['objects']) > 0:
             for bb in dicom_slice['objects']:
@@ -29,44 +34,44 @@ def get_redaction_bboxes_and_metadata(labels: List[Dict]) -> Tuple[List[Dict], L
                     h = dicom_slice['metadata']['height']
                     x1 = round(bb['boundingBox']['x'] * w)
                     y1 = round(bb['boundingBox']['y'] * h)
-                    bboxes.append({
-                        'x1': x1,
-                        'y1': y1,
-                        'x2': x1 + round(bb['boundingBox']['w'] * w),
-                        'y2': y1 + round(bb['boundingBox']['h'] * h)
-                    })
+                    bboxes.append(
+                        {
+                            'x1': x1,
+                            'y1': y1,
+                            'x2': x1 + round(bb['boundingBox']['w'] * w),
+                            'y2': y1 + round(bb['boundingBox']['h'] * h),
+                        }
+                    )
     return bboxes, metadata
 
 
-def redact_slice(bboxes: List[Dict], meta: Dict, output_dirname: str, output_filename: str) -> FileDataset:
+def redact_slice(
+    bboxes: List[Dict], meta: Dict, output_dirname: str, output_filename: str
+) -> FileDataset:
     r = requests.get(meta['signed_url'])
     with open(os.path.join(output_dirname, output_filename), 'wb') as f:
         f.write(r.content)
-    dcm = pydicom.read_file(
-        os.path.join(output_dirname, output_filename)
-    )
-    if (
-            dcm.file_meta.TransferSyntaxUID == pydicom.uid.JPEG2000Lossless
-            and dcm.BitsStored == 12
-    ):
+    dcm = read_file(os.path.join(output_dirname, output_filename))
+    if dcm.file_meta.TransferSyntaxUID == JPEG2000Lossless and dcm.BitsStored == 12:
         dcm.BitsStored = 16
     for bbox in bboxes:
         # Zero out pixels inside bounding box
-        dcm.pixel_array[bbox['y1']:bbox['y2'], bbox['x1']:bbox['x2']] = 0
-        if (
-                dcm.file_meta.TransferSyntaxUID
-                == pydicom.uid.JPEG2000Lossless
-        ):
+        dcm.pixel_array[bbox['y1'] : bbox['y2'], bbox['x1'] : bbox['x2']] = 0
+        if dcm.file_meta.TransferSyntaxUID == JPEG2000Lossless:
             encoded = jpeg2k_encode(dcm.pixel_array, level=0)
-            dcm.PixelData = pydicom.encaps.encapsulate([encoded])
+            dcm.PixelData = encapsulate([encoded])
         else:
             redacted_pixeldata = dcm.pixel_array.tobytes()
             dcm.PixelData = redacted_pixeldata
     return dcm
 
 
-def main(keyfile: str, project_hashes: List[str], bucket_name: str, bucket_folder: str) -> None:
-    user_client = EncordUserClient.create_with_ssh_private_key(ssh_private_key_path=keyfile)
+def main(
+    keyfile: str, project_hashes: List[str], bucket_name: str, bucket_folder: str
+) -> None:
+    user_client = EncordUserClient.create_with_ssh_private_key(
+        ssh_private_key_path=keyfile
+    )
     client = boto3.client("s3")
 
     output_path = './dicom/'
